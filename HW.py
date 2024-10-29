e44300a62f19eca8fbbe5988caae6f1ee95a4f67
AlekseyPuchkin <toporbox@gmail.com>
2024-10-29 13:32:45 +0300

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,7 @@
+print('Hello, World!')
+print(type('Hello, world!'))
+print(2 + 3)
+print(type(2 + 2))
+print(2 + 2 > 2 + 3, 2 + 2 < 2 + 3)
+print(2 + 2 > 2 + 3 or 2 + 2 < 2 + 3, 'Hello, World!')
+    
